fd45c6965681b12494b3c4f7c707178e34b757ce
Janusz 'Kali' Kaliszczak <kali187@gmail.com>
2025-01-19 00:53:54 +0000

    Mini Surround plugin config

--- lua/plugin-configs/mini.lua
+++ lua/plugin-configs/mini.lua
@@ -28,20 +28,8 @@ require('mini.move').setup({
 })
 require('mini.operators').setup()
 require('mini.pairs').setup()
-require('mini.surround').setup({
-	mappings = {
-		add = '<leader>sa',          -- Add surrounding in Normal and Visual modes
-		delete = '<leader>sd',       -- Delete surrounding
-		find = '<leader>sf',         -- Find surrounding (to the right)
-		find_left = '<leader>sF',    -- Find surrounding (to the left)
-		highlight = '<leader>sh',    -- Highlight surrounding
-		replace = '<leader>sr',      -- Replace surrounding
-		update_n_lines = '<leader>sn', -- Update `n_lines`
-
-		suffix_last = 'l',           -- Suffix to search with :"prev": method
-		suffix_next = 'n',           -- Suffix to search with "next" method
-	},
-})
+require('mini.surround').setup()
+-- *Something* to "test" *surround*
 require('mini.splitjoin').setup()
 local hipatterns = require('mini.hipatterns')
 hipatterns.setup({
