0a2550d7810301e7074d32045c881b373a63732a
Janusz 'Kali' Kaliszczak <kali187@gmail.com>
2025-06-19 13:20:13 +0100

    Added Lualine to replace galaxyline and fixed some noice issues

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -32,17 +32,23 @@ local plugins = { -- Editor support.
 		vim.keymap.set({ "n", "v" }, "<leader>xe", require("nvim-emmet").wrap_with_abbreviation)
 	end
 },
-
 	{
-		'glepnir/galaxyline.nvim',
-		branch = 'main',
-		-- your statusline
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
 		config = function()
-			require('plugin-configs.galaxyline')
-		end,
-		--           -- some optional icons
-		requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+			require "plugin-configs.lualine-evil"
+		end
 	},
+	-- {
+	-- 	'glepnir/galaxyline.nvim',
+	-- 	branch = 'main',
+	-- 	-- your statusline
+	-- 	config = function()
+	-- 		require('plugin-configs.galaxyline')
+	-- 	end,
+	-- 	--           -- some optional icons
+	-- 	requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+	-- },
 	{
 		"b0o/incline.nvim",
 		config = function()
@@ -86,44 +92,100 @@ local plugins = { -- Editor support.
 		require "plugin-configs.treesitter"
 	end
 }, "nvim-treesitter/nvim-treesitter-textobjects", "editorconfig/editorconfig-vim",
+
+
 	{
-		'hrsh7th/cmp-cmdline',
-		enabled = true,
-		lazy = true,
-		event = { 'InsertEnter', 'CmdlineEnter' },
-		dependencies = {
-			'hrsh7th/nvim-cmp',
-			'hrsh7th/cmp-buffer',
-			'hrsh7th/cmp-path',
+		'saghen/blink.cmp',
+		-- optional: provides snippets for the snippet source
+		dependencies = { 'rafamadriz/friendly-snippets' },
+
+		-- use a release tag to download pre-built binaries
+		version = '1.*',
+		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+		-- build = 'cargo build --release',
+		-- If you use nix, you can build from source using latest nightly rust with:
+		-- build = 'nix run .#build-plugin',
+
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+			-- 'super-tab' for mappings similar to vscode (tab to accept)
+			-- 'enter' for enter to accept
+			-- 'none' for no mappings
+			--
+			-- All presets have the following mappings:
+			-- C-space: Open menu or open docs if already open
+			-- C-n/C-p or Up/Down: Select next/previous item
+			-- C-e: Hide menu
+			-- C-k: Toggle signature help (if signature.enabled = true)
+			--
+			-- See :h blink-cmp-config-keymap for defining your own keymap
+			keymap = { preset = 'enter' },
+
+			appearance = {
+				-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+				-- Adjusts spacing to ensure icons are aligned
+				nerd_font_variant = 'mono'
+			},
+
+			-- (Default) Only show the documentation popup when manually triggered
+			completion = { documentation = { auto_show = true } },
+
+			-- Default list of enabled providers defined so that you can extend it
+			-- elsewhere in your config, without redefining it, due to `opts_extend`
+			sources = {
+				default = { 'lsp', 'path', 'snippets', 'buffer' },
+			},
+
+			-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+			-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+			-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+			--
+			-- See the fuzzy documentation for more information
+			fuzzy = { implementation = "prefer_rust_with_warning" }
 		},
-		config = function()
-			require "plugin-configs.cmp-cmdline"
-		end,
+		opts_extend = { "sources.default" }
 	},
+	-- {
+	-- 	'hrsh7th/cmp-cmdline',
+	-- 	enabled = true,
+	-- 	lazy = true,
+	-- 	event = { 'InsertEnter', 'CmdlineEnter' },
+	-- 	dependencies = {
+	-- 		'hrsh7th/nvim-cmp',
+	-- 		'hrsh7th/cmp-buffer',
+	-- 		'hrsh7th/cmp-path',
+	-- 	},
+	-- 	config = function()
+	-- 		require "plugin-configs.cmp-cmdline"
+	-- 	end,
+	-- },
+	-- {
+	-- 	"hrsh7th/nvim-cmp",
+	-- 	config = function()
+	-- 		require "plugin-configs.cmp"
+	-- 	end,
+	-- 	dependencies = { "hrsh7th/cmp-nvim-lsp", "onsails/lspkind-nvim", "SergioRibera/cmp-dotenv", "hrsh7th/cmp-path",
+	-- 		"hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lua", "hrsh7th/cmp-cmdline",
+	-- 		"hrsh7th/cmp-nvim-lsp-document-symbol", "hrsh7th/cmp-nvim-lsp-signature-help", "petertriho/cmp-git",
+	-- 		"rcarriga/cmp-dap", {
+	-- 		"nvim-telescope/telescope-fzf-native.nvim",
+	-- 		build = "make"
+	-- 	}, {
+	-- 		"tzachar/fuzzy.nvim",
+	-- 		dependencies = { "nvim-telescope/telescope-fzf-native.nvim" }
+	-- 	}, {
+	-- 		"tzachar/cmp-fuzzy-path",
+	-- 		dependencies = "tzachar/fuzzy.nvim"
+	-- 	} }
+	-- },
 	{
-		"hrsh7th/nvim-cmp",
+		"b0o/mapx.nvim",
 		config = function()
-			require "plugin-configs.cmp"
-		end,
-		dependencies = { "hrsh7th/cmp-nvim-lsp", "onsails/lspkind-nvim", "SergioRibera/cmp-dotenv", "hrsh7th/cmp-path",
-			"hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lua", "hrsh7th/cmp-cmdline",
-			"hrsh7th/cmp-nvim-lsp-document-symbol", "hrsh7th/cmp-nvim-lsp-signature-help", "petertriho/cmp-git",
-			"rcarriga/cmp-dap", {
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make"
-		}, {
-			"tzachar/fuzzy.nvim",
-			dependencies = { "nvim-telescope/telescope-fzf-native.nvim" }
-		}, {
-			"tzachar/cmp-fuzzy-path",
-			dependencies = "tzachar/fuzzy.nvim"
-		} }
-	}, {
-	"b0o/mapx.nvim",
-	config = function()
-		require "keyboard-mappings"
-	end
-}, "f-person/git-blame.nvim", {
+			require "keyboard-mappings"
+		end
+	}, "f-person/git-blame.nvim", {
 	"lewis6991/gitsigns.nvim",
 	config = function()
 		require("gitsigns").setup {}
